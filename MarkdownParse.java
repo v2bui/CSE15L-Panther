d91ef3b02efd0b4250b5b5b12aa65bd41dbe7ed1
amquach00 <76972833+amquach00@users.noreply.github.com>
2022-01-26 13:58:49 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -20,7 +20,7 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
 		
-	    if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen) {
+	    if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
 		    return toReturn;
 	    }
             
